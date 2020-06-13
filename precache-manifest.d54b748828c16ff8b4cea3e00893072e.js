c937e2126ac76a9d6eb8f5654dd1631130bddc52
Muhammad Sameer <itsmdsameerkhan@gmail.com>
2020-06-13 20:12:39 +0545

    Updates

---
+++ precache-manifest.d54b748828c16ff8b4cea3e00893072e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95e75d74078620521ff0ecc2f6dfccd5",
+    "url": "/geoloc-weather/index.html"
+  },
+  {
+    "revision": "7ad510d157275cdabd64",
+    "url": "/geoloc-weather/static/css/main.5a41fc1f.chunk.css"
+  },
+  {
+    "revision": "1a872173b2ff33e95b90",
+    "url": "/geoloc-weather/static/js/2.e857a365.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/geoloc-weather/static/js/2.e857a365.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ad510d157275cdabd64",
+    "url": "/geoloc-weather/static/js/main.6a067b7f.chunk.js"
+  },
+  {
+    "revision": "ee24ef8b9ff035508e96",
+    "url": "/geoloc-weather/static/js/runtime-main.02c19216.js"
+  }
+]);
